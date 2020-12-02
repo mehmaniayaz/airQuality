cad143979c1d5a239132b4b25b60bda69ef9e64a
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-12-02 13:00:13 -0600

    artifact for plotting model metric as a function of parameter grid is plotted

---
+++ src/artifacts.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def param_grid_evaluation(model, param_grid, metric):
+    """
+    plot the metric as a function of parameter grid values
+    :param model: fit model with cv_results_ as attribute
+    :param param_grid: dictionary of parameter grids used to train the model
+    :return:
+    """
+    df_paramgrid = pd.DataFrame(model.cv_results_)
+    params = ['param_' + x for x in list(param_grid.keys())]
+    features = params + [metric]
+    for i in features:
+        plt.figure()
+        sns.scatterplot(x=i, y='mean_test_score', data=df_paramgrid)
