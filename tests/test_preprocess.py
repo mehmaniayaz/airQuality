097b159691bae48e23e3c93a965a2ad1cbd0bf53
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-12-01 21:55:35 -0600

    passing test for finding time of day

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,1 +1,10 @@
-import
+import unittest
+import datetime
+from src.preprocess import *
+
+
+class TestPreprocess(unittest.TestCase):
+    def test_find_time_of_day(self):
+        example = pd.DataFrame([datetime.datetime.strptime('18-30-00', '%H-%M-%S')])
+        example_time_of_day = find_time_of_day(x=example.iloc[0,0])
+        self.assertEquals(example_time_of_day, 'evening')
