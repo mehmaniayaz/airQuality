b4cee966122c4f5b90548f65a568ea9fec4ee7b3
Ayaz Mehmani <mehmani.ayaz@gmail.com>
2020-12-02 13:17:42 -0600

    artifacts for feature importance added

--- src/artifacts.py
+++ src/artifacts.py
@@ -16,3 +16,26 @@ def param_grid_evaluation(model, param_grid, metric):
     for i in features:
         plt.figure()
         sns.scatterplot(x=i, y=metric, data=df_paramgrid)
+
+
+def visualize_n_important_features(df, n, model, target):
+    """
+
+    :param df: dataframe that has been used for training
+    :param n: the first n important features
+    :param model: random forest model
+    :param target: list target feature for model
+    :return:
+    """
+
+    if not isinstance(target, list):
+        raise
+    dict_feature_importance = dict(zip(list(df.keys()), model.best_estimator_.feature_importances_))
+    dict_feature_importance_sorted = {k: v for k, v in
+                                      sorted(dict_feature_importance.items(), key=lambda item: item[1], reverse=True)}
+
+    plt.figure()
+    sns.pairplot(df[list(dict_feature_importance_sorted.keys())[0:n] + target])
+
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(df[list(dict_feature_importance_sorted.keys())[0:n] + ['total-chemicals']].corr(), annot=True)
